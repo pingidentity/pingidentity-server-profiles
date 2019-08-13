698efc7184ebdd10d01ae628058088e11c1c9195
Chris Leggett <cleggett@pingidentity.com>
2019-08-13 09:50:14 -0400

    Added PA-Clustering

---
+++ pa-clustering/pingaccess/hooks/50-before-post-start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+#
+# Ping Identity DevOps - Docker Build Hooks
+#
+#- This is called after the start or restart sequence has finished and before 
+#- the server within the container starts
+#
+
+# shellcheck source=pingcommon.lib.sh
+. "${HOOKS_DIR}/pingcommon.lib.sh"
+
+echo `set`
+
+echo_green 'Checking if OPERATIONAL_MODE is set...'
+echo "OPERATIONAL_MODE:"${OPERATIONAL_MODE}
+
+if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE" ]]; then
+  echo "Shutting down the eth01 interface..."
+  ip link set eth0 down
+fi
+
+if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE" ]]; then
+  echo "Adding engine..."
+  run_hook "51-add-engine.sh"
+fi
---
+++ pa-clustering/pingaccess/hooks/80-post-start.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+#
+# Ping Identity DevOps - Docker Build Hooks
+#
+#- This script is used to import any configurations that are
+#- needed after PingAccess starts
+
+# shellcheck source=pingcommon.lib.sh
+. "${HOOKS_DIR}/pingcommon.lib.sh"
+
+if test ${RUN_PLAN} = "START" ; then
+  echo "Check for configuration to import.."
+  if ! test -f ${STAGING_DIR}/instance/conf/pa.jwk ; then
+    echo "INFO: No 'pa.jwk' found in /instance/conf"
+    if ! test -f ${STAGING_DIR}/instance/data/data.json ; then
+      echo "INFO: No file named 'data.json' found in /instance/data"
+      echo "INFO: skipping config import"
+    fi
+  else 
+    if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE" ]]; then
+      run_hook "81-import-initial-configuration.sh"
+    fi   
+  fi
+fi
+
+if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE" ]]; then
+  echo "Bringing eth0 back up..."
+  ip link set eth0 up
+fi 
