57bea51159411deb395b0d72638e7fddc06cb309
Chris Leggett <cleggett@pingidentity.com>
2019-10-30 12:40:46 -0400

    Fixed INITIAL_ADMIN_PASSWORD

--- pa-clustering/pingaccess/hooks/51-add-engine.sh
+++ pa-clustering/pingaccess/hooks/51-add-engine.sh
@@ -31,7 +31,6 @@ if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE" ]
 
     # Get Engine Certificate ID
     echo "Retrieving Key Pair ID from administration API..."
-    echo "INITIAL_ADMIN_PASSWORD:"${INITIAL_ADMIN_PASSWORD}
     keypairid=$( curl -v -k -u Administrator:"${INITIAL_ADMIN_PASSWORD}" -H "X-Xsrf-Header: PingAccess" https://${pahost}:9000/pa-admin-api/v3/httpsListeners | jq '.items[] | select(.name=="CONFIG QUERY") | .keyPairId' )
     echo "KeyPairId:"${keypairid}
 
