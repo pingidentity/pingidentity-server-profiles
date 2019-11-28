57d6a5350daf43e251cda8044e6b3a437484fb65
Savitha Ganapathi <savithaganapathi@pingidentity.com>
2019-11-28 17:21:24 -0600

    Mark script executable for all

--- baseline/pingdirectory/hooks/82-upload-csd-s3.sh
+++ baseline/pingdirectory/hooks/82-upload-csd-s3.sh
@@ -2,7 +2,7 @@
 
 ${VERBOSE} && set -x
 
-# Set PATH - since this is executed from within the server process, it may not have all we need on the path.
+# Set PATH - since this is executed from within the server process, it may not have all we need on the path
 export PATH="${PATH}:${SERVER_ROOT_DIR}/bin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${JAVA_HOME}/bin"
 
 # Allow overriding the log archive URL with an arg
