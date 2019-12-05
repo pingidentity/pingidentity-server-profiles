d89a86dbcfd24b0b4ce0a7f46108eb5a01f1fd8d
calvincarter-ping <calvincarter@pingidentity.com>
2019-12-05 12:52:15 -0600

    add wait time per api request

--- pingaccess-cluster/hooks/utils.lib.sh
+++ pingaccess-cluster/hooks/utils.lib.sh
@@ -8,7 +8,7 @@
 ########################################################################################################################
 function make_api_request
 {
-    curl -k --retry ${API_RETRY_LIMIT} --retry-delay 1 --retry-connrefuse -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess " "$@"
+    curl -k --retry ${API_RETRY_LIMIT} --max-time ${API_TIMEOUT_WAIT} --retry-delay 1 --retry-connrefuse -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess " "$@"
     if [[ ! $? -eq 0 ]]; then
         echo "Admin API connection refused"
         exit 1
