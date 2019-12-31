55dbc5eb1ebb9594a3887e908711ef1611a10266
calvincarter <calvincarter.160@gmail.com>
2019-12-31 15:10:36 -0600

    update environment variable name  K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN

--- pingaccess-cluster/hooks/51-add-engine.sh
+++ pingaccess-cluster/hooks/51-add-engine.sh
@@ -18,7 +18,7 @@
 if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE" ]]; then
     echo "This node is an engine..."
     while true; do
-        curl -ss --silent -o /dev/null -k https://${K8S_STATEFUL_SET_SERVICE_NAME_PA}:9090/pa/heartbeat.ping
+        curl -ss --silent -o /dev/null -k https://${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN}:9090/pa/heartbeat.ping
         if ! test $? -eq 0 ; then
             echo "Adding Engine: Server not started, waiting.."
             sleep 3
@@ -29,17 +29,17 @@ if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE" ]
     done
 
     # Retrieving CONFIG QUERY id
-    OUT=$( make_api_request https://${K8S_STATEFUL_SET_SERVICE_NAME_PA}:9000/pa-admin-api/v3/httpsListeners )
+    OUT=$( make_api_request https://${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN}:9000/pa-admin-api/v3/httpsListeners )
     CONFIG_QUERY_LISTENER_KEYPAIR_ID=$( jq -n "$OUT" | jq '.items[] | select(.name=="CONFIG QUERY") | .keyPairId' )
     echo "CONFIG_QUERY_LISTENER_KEYPAIR_ID:${CONFIG_QUERY_LISTENER_KEYPAIR_ID}"
 
     echo "Retrieving the Key Pair alias..."
-    OUT=$( make_api_request https://${K8S_STATEFUL_SET_SERVICE_NAME_PA}:9000/pa-admin-api/v3/keyPairs  )
+    OUT=$( make_api_request https://${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN}:9000/pa-admin-api/v3/keyPairs  )
     KEYPAIR_ALIAS_NAME=$( jq -n "$OUT" | jq -r '.items[] | select(.id=='${CONFIG_QUERY_LISTENER_KEYPAIR_ID}') | .alias' )
     echo "KEYPAIR_ALIAS_NAME:"${KEYPAIR_ALIAS_NAME}
 
     # Retrieve Engine Cert ID
-    OUT=$( make_api_request https://${K8S_STATEFUL_SET_SERVICE_NAME_PA}:9000/pa-admin-api/v3/engines/certificates )
+    OUT=$( make_api_request https://${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN}:9000/pa-admin-api/v3/engines/certificates )
     ENGINE_CERT_ID=$( jq -n "$OUT" | \
                       jq --arg KEYPAIR_ALIAS_NAME "${KEYPAIR_ALIAS_NAME}" \
                       '.items[] | select(.alias==$KEYPAIR_ALIAS_NAME and .keyPair==true) | .id' )
@@ -47,7 +47,7 @@ if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE" ]
 
     # Retrieve Engine ID
     SHORT_HOST_NAME=$(hostname)
-    OUT=$( make_api_request https://${K8S_STATEFUL_SET_SERVICE_NAME_PA}:9000/pa-admin-api/v3/engines )
+    OUT=$( make_api_request https://${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN}:9000/pa-admin-api/v3/engines )
     ENGINE_ID=$( jq -n "$OUT" | \
                  jq --arg SHORT_HOST_NAME "${SHORT_HOST_NAME}" \
                  '.items[] | select(.name==$SHORT_HOST_NAME) | .id' )
@@ -58,14 +58,14 @@ if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE" ]
             \"name\":\"${SHORT_HOST_NAME}\",
             \"selectedCertificateId\": ${ENGINE_CERT_ID},
             \"configReplicationEnabled\": true
-        }" https://${K8S_STATEFUL_SET_SERVICE_NAME_PA}:9000/pa-admin-api/v3/engines )
+        }" https://${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN}:9000/pa-admin-api/v3/engines )
         ENGINE_ID=$( jq -n "$OUT" | jq '.id' )
     fi
 
     # Download Engine Configuration
     echo "ENGINE_ID:"${ENGINE_ID}
     echo "Retrieving the engine config"
-    make_api_request -X POST https://${K8S_STATEFUL_SET_SERVICE_NAME_PA}:9000/pa-admin-api/v3/engines/${ENGINE_ID}/config \
+    make_api_request -X POST https://${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN}:9000/pa-admin-api/v3/engines/${ENGINE_ID}/config \
     -o engine-config.zip
 
     echo "Extracting config files to conf folder..."
