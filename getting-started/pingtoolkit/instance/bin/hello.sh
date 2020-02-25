91bec091d6e0b417645ee3a983586d3c39de336e
Terry Sigle <terrysigle@pingidentity.com>
2020-02-25 09:26:56 -0600

    Example script

---
+++ getting-started/pingtoolkit/instance/bin/hello.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo "
+########################################
+# Hello from the PingToolkit Container!
+#
+#     Date: $(date)
+# Hostname: $(hostname)
+########################################
+"
