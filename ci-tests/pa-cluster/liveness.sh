1c442a36f9e4ed9b391433418d5a298b5e7a6919
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-03-12 13:53:58 -0500

    rando-echo

--- ci-tests/pa-cluster/liveness.sh
+++ ci-tests/pa-cluster/liveness.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+echo "THIS IS LIVENESS@!!"
 wait-for "${PA_ENGINE_PUBLIC_HOSTNAME}:3000" -t 200 || exit 1
 sleep 5
 curl -ks -u "Administrator:2FederateM0re" -H "Content-Type: application/json" -H "X-Xsrf-Header: PingAccess" https://localhost:9000/pa-admin-api/v3/engines | jq '.items[0].configReplicationEnabled' | grep true
