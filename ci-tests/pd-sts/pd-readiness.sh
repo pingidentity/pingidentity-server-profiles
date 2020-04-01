8e5ce036611bcb5540897c7c78ae9b00e3c353d6
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-04-01 12:12:09 -0500

    update

--- ci-tests/pd-sts/pd-readiness.sh
+++ ci-tests/pd-sts/pd-readiness.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
-ready=$(kubectl get sts/pingdirectory | grep pingdirectory | awk '{ print $2}' | awk -F'\' '{ print $1}')
-requested=$(kubectl get sts/pingdirectory | grep pingdirectory | awk '{ print $2}' | awk -F'\' '{ print $2}')
-test "$ready" = "$requested" ; 
+while true ; do 
+  ready=$(kubectl get sts/pingdirectory | grep pingdirectory | awk '{ print $2}' | awk -F'\' '{ print $1}')
+  requested=$(kubectl get sts/pingdirectory | grep pingdirectory | awk '{ print $2}' | awk -F'\' '{ print $2}')
+  test "$ready" = "$requested" && break
+  echo "not ready yet"
+  sleep 5
+done
